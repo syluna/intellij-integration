129c1e37487d1bc636a774a008cf3e5f63002315
James Khan <jameskhan1983@gmail.com>
2019-08-29 11:10:12 +0100

    Don't allow showing the JME panel until the indices have been built.

--- src/main/java/com/jmonkeystore/ide/editor/JmeModelEditorProvider.java
+++ src/main/java/com/jmonkeystore/ide/editor/JmeModelEditorProvider.java
@@ -3,6 +3,7 @@ package com.jmonkeystore.ide.editor;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorPolicy;
 import com.intellij.openapi.fileEditor.FileEditorProvider;
+import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.jmonkeystore.ide.editor.impl.JmeModelFileEditorImpl;
@@ -11,13 +12,13 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
-public class JmeModelEditorProvider implements FileEditorProvider {
+public class JmeModelEditorProvider implements FileEditorProvider, DumbAware {
 
     @NonNls
     private static final String EXTENSION = "j3o";
 
     @NonNls
-    private static final String EDITOR_TYPE_ID = "yumlmeDiagrams";
+    private static final String EDITOR_TYPE_ID = "JmeSceneEditor";
 
     @Override
     public boolean accept(@NotNull Project project, @NotNull VirtualFile virtualFile) {
