3811fd8e657e66ef6d6c59bde3f3a41048adc78a
jayfella <jayfella@gmail.com>
2019-12-06 13:21:55 +0000

    Add ParticleEmitter to context menu.

--- src/main/java/com/jmonkeystore/ide/scene/explorer/impl/SceneTreeContextMenuListener.java
+++ src/main/java/com/jmonkeystore/ide/scene/explorer/impl/SceneTreeContextMenuListener.java
@@ -6,11 +6,15 @@ import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.treeStructure.Tree;
 import com.jme3.asset.AssetManager;
+import com.jme3.effect.ParticleEmitter;
+import com.jme3.effect.ParticleMesh;
 import com.jme3.environment.EnvironmentCamera;
 import com.jme3.environment.LightProbeFactory;
 import com.jme3.environment.generation.JobProgressAdapter;
 import com.jme3.light.*;
 import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
@@ -269,6 +273,36 @@ public class SceneTreeContextMenuListener implements MouseListener {
         });
         geomsMenu.add(boxMenuItem);
         menu.add(geomsMenu);
+
+        JMenuItem particleEmitterMenuItem = new JMenuItem("Particle Emitter");
+        particleEmitterMenuItem.addActionListener(e -> {
+
+            AssetManager assetManager = ServiceManager.getService(JmeEngineService.class).getAssetManager();
+
+            ParticleEmitter particleEmitter = new ParticleEmitter("New ParticleEmitter", ParticleMesh.Type.Triangle, 30);
+
+            Material mat_red = new Material(assetManager, "Common/MatDefs/Misc/Particle.j3md");
+            mat_red.setTexture("Texture", assetManager.loadTexture("Effects/Explosion/flame.png"));
+
+            particleEmitter.setMaterial(mat_red);
+            particleEmitter.setImagesX(2);
+            particleEmitter.setImagesY(2); // 2x2 texture animation
+            particleEmitter.setEndColor(  new ColorRGBA(1f, 0f, 0f, 1f));   // red
+            particleEmitter.setStartColor(new ColorRGBA(1f, 1f, 0f, 0.5f)); // yellow
+            particleEmitter.getParticleInfluencer().setInitialVelocity(new Vector3f(0, 2, 0));
+            particleEmitter.setStartSize(1.5f);
+            particleEmitter.setEndSize(0.1f);
+            particleEmitter.setGravity(0, 0, 0);
+            particleEmitter.setLowLife(1f);
+            particleEmitter.setHighLife(3f);
+            particleEmitter.getParticleInfluencer().setVelocityVariation(0.3f);
+
+            Node node = (Node) clickedNode.getUserObject();
+            node.attachChild(particleEmitter);
+            ServiceManager.getService(SceneExplorerService.class).refreshScene();
+        });
+        menu.add(particleEmitterMenuItem);
+
 
         return menu;
     }
