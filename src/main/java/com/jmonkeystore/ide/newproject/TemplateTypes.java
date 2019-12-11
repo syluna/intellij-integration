b679abc670f7fb3b2f0e8b3c21d4624269e59177
jayfella <jayfella@gmail.com>
2019-12-11 09:52:47 +0000

    Create some static template type names.

---
+++ src/main/java/com/jmonkeystore/ide/newproject/TemplateTypes.java
@@ -0,0 +1,8 @@
+package com.jmonkeystore.ide.newproject;
+
+public class TemplateTypes {
+
+    public static final String BASIC = "Basic";
+    public static final String FPS = "First Person Shooter";
+
+}
