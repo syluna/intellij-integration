995bd142fda261a840256c0297c03be3ee5951da
jayfella <jayfella@gmail.com>
2019-12-11 08:53:53 +0000

    Generify interface

--- src/main/java/com/jmonkeystore/ide/editor/controls/JmeEditorControl.java
+++ src/main/java/com/jmonkeystore/ide/editor/controls/JmeEditorControl.java
@@ -1,7 +1,7 @@
 package com.jmonkeystore.ide.editor.controls;
 
-import com.jmonkeystore.ide.editor.objects.JmeObject;
 
+import com.jmonkeystore.ide.api.JmeObject;
 
 public interface JmeEditorControl extends JmeObject {
 
